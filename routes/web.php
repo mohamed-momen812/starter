768102c2a11d00c4ae4a01c78c847fd1b91292c1
Mohamed Momen <mohamedmomen81296@gmail.com>
2024-12-26 12:59:00 +0200

    no thing

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PaypalController;
 
 Route::get('/', function () {
-    return view('welcome'); 
+    return view('welcome');
 });
 
 Route::post('paypal', [PaypalController::class, 'paypal'])->name('paypal');
