e0f8a2ffa0a7044c54d1b0faf6692c73ed16315f
Mohamed Momen <mohamedmomen81296@gmail.com>
2025-01-14 17:31:21 +0200

    finshing webscrabing

--- app/Console/Commands/ScrapeProductPrice.php
+++ app/Console/Commands/ScrapeProductPrice.php
@@ -16,9 +16,9 @@ class ScrapeProductPrice extends Command
         $this->info("Searching for product:  $productName");
 
         $websites = [
-            // 'https://www.ebay.com',
-            // 'https://www.amazon.com',
-           'https://alkhunaizan.sa'
+            'https://www.ebay.com',
+            'https://alkhunaizan.sa',
+            'https://alassly.com'
         ];
 
         foreach ($websites as $website) {
@@ -45,13 +45,7 @@ class ScrapeProductPrice extends Command
         $crawler = $client->request('GET', $searchUrl);
 
         // Update the selector based on the website's structure
-        $productDetails = $crawler->filter('.product-item-details')->each(function ($node) {
-            return [
-                'name' => $node->filter('.product-item-name')->text(),
-                'price' => $node->filter('.price-final_price')->text(),
-                'link' => $node->filter('a')->attr('href'),
-            ];
-        });
+        $productDetails = $this->extractProductDetails($crawler, $website);
 
         if (!empty($productDetails)) {
             foreach ($productDetails as $product) {
@@ -69,14 +63,55 @@ class ScrapeProductPrice extends Command
         // Customize search URLs based on the website's format
         $query = urlencode($productName);
 
-        if (strpos($website, 'amazon') !== false) {
-            return "$website/s?k=$query"; // Amazon's search pattern
-        } elseif (strpos($website, 'ebay') !== false) {
+       if (strpos($website, 'ebay') !== false) {
+
             return "$website/sch/i.html?_nkw=$query"; // eBay's search pattern
-        } elseif (strpos($website, 'alkhunaizan.sa') !== false) {
+
+        } elseif (strpos($website, 'alkhunaizan') !== false) {
+
             return "$website/catalogsearch/result/?q=$query"; // alkhunaizan's search pattern
+
+        } elseif (strpos($website, 'alassly') !== false) {
+
+            return "$website/?s=$query&post_type=product"; // alassly's search pattern
+
         } else {
+            
             return "$website/search?q=$query"; // Default search pattern
+        } 
+    }
+
+    private function extractProductDetails($crawler, $website)
+    {
+        // Extract product details based on the website's structure
+        $productDetails = [];
+
+       if (strpos($website, 'ebay') !== false) {
+            $productDetails = $crawler->filter('.s-item__info')->each(function ($node) {
+                return [
+                    'name' => $node->filter('.s-item__title')->text(),
+                    'price' => $node->filter('span.s-item__price')->text(),
+                    'link' => $node->filter('a.s-item__link')->attr('href')
+                ];
+            });
+        } elseif (strpos($website, 'alkhunaizan') !== false) {
+            $productDetails = $crawler->filter('.product-list-container')->each(function ($node) {
+                return [
+                    'name' => $node->filter('.product-item-link')->text(),
+                    'price' => $node->filter('.price-final_price')->text(),
+                    'link' => $node->filter('.product-item-link')->attr('href')
+                ];
+            });
+        } elseif (strpos($website, 'alassly') !== false) {
+            $productDetails = $crawler->filter('.product-wrapper')->each(function ($node) {
+                return [
+                    'name' => $node->filter('.wd-entities-title')->text(),
+                    'price' => $node->filter('.price')->text(),
+                    'link' => $node->filter('a')->attr('href')
+                ];
+            });
         }
+
+        return $productDetails;
     }
 }
